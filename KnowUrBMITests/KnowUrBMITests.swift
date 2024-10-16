f5e77a632683292862b4857ed50cf3bae039e17d
Reuben Simphiwe Kuse <sim.khuse@gmail.com>
2024-10-16 12:10:05 +0200

    Initial Commit

---
+++ KnowUrBMITests/KnowUrBMITests.swift
@@ -0,0 +1,17 @@
+//
+//  KnowUrBMITests.swift
+//  KnowUrBMITests
+//
+//  Created by Reuben Simphiwe Kuse on 2024/10/16.
+//
+
+import Testing
+@testable import KnowUrBMI
+
+struct KnowUrBMITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

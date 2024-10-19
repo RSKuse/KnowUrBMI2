3456cc6159ef979a253db030c33839e32abdbfa0
Reuben Simphiwe Kuse <sim.khuse@gmail.com>
2024-10-19 05:32:01 +0200

    modifified the /Users/gugulethumhlanga/Documents/learning/KnowUrBMI2/KnowUrBMI.xcodeproj/project.pbxproj for pods could be added

--- Podfile
+++ Podfile
@@ -1,8 +1,10 @@
 platform :ios, '13.0'
-project 'KnowUrBMI.xcodeproj'
+
+project 'KnowUrBMI.xcodeproj' # This line is essential
 
 target 'KnowUrBMI' do
   use_frameworks!
-  pod 'Charts'
 
+  # Pods for KnowUrBMI
+  pod 'Charts'
 end
---
+++ build/Pods.build/Debug-iphoneos/Pods-KnowUrBMI.build/DerivedSources/Pods_KnowUrBMI_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_KnowUrBMIVersionString[];
+ extern const double Pods_KnowUrBMIVersionNumber;
+
+ const unsigned char Pods_KnowUrBMIVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_KnowUrBMI  PROJECT:Pods-1" "\n";
+ const double Pods_KnowUrBMIVersionNumber __attribute__ ((used)) = (double)1.;
